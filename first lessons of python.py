e07653a598a39f9049bdfaa65c21ba35985d8944
Barbara-S5 <vsankova398@gmail.com>
2024-05-26 23:37:37 +0700

    New update ab переменных

--- first lessons of python.py
+++ first lessons of python.py
@@ -1,5 +1,18 @@
-print("Привет, я Анфиса!")
-print('Я маленькая, но уже программа!')
+#print("Привет, я Анфиса!")
+#print('Я маленькая, но уже программа!')
 
-Message = "Guten Tag"
-print(Message)
+#Message = "Guten Tag"
+#print(Message)
+
+name = "Барбора"
+job = "персональный помощник"
+print("Привет, я " + name + ", твой " + job + "!")
+
+#Числовые значения без кавычек
+TF = 25
+Th = 3
+total = TF + Th
+print(total)
+
+summa = "25"
+print("20 плюс 5 будет " + summa + "")
