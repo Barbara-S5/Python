ad95554c4602b3e14cb0b8a2bb56332483674cc6
Barbara-S5 <vsankova398@gmail.com>
2024-05-29 13:56:20 +0700

    if/else условия

--- second topic of python.py
+++ second topic of python.py
@@ -1,8 +1,7 @@
 #Циклы
-bremen_musicians = ['Кот', 'Пёс', 'Трубадур', 'Осёл', 'Петух']
-
-for music_band in bremen_musicians:
-    print(music_band)
+#bremen_musicians = ['Кот', 'Пёс', 'Трубадур', 'Осёл', 'Петух']
+#for music_band in bremen_musicians:
+ #   print(music_band)
 
 #kids = ['Витя', 'Маша', 'Марина']
 #sweets = ['Батончик', 'Сникерс', 'Мишка Косолапый', 'Коровка']
@@ -12,19 +11,25 @@ for music_band in bremen_musicians:
         #print(kid, 'get', sweet)
 
 #В range не включается последнее число диапазона
-diapozons = range(1, 5)
-for diapozon in diapozons:
-    print(diapozon)
+#diapozons = range(1, 5)
+#for diapozon in diapozons:
+#    print(diapozon)
 
-for new_year in reversed(range(1,13)):
-    print(new_year)
+#for new_year in reversed(range(1,13)):
+#    print(new_year)
 
-print("Ура! Новый год!")
+#print("Ура! Новый год!")
 
-print("Саня строил дом")
-print("Первый этаж готов")
+#print("Саня строил дом")
+#print("Первый этаж готов")
 
-for i in range(2, 10):
-    print(str(i) + " этаж выше, чем " + str(i-1))
+#for i in range(2, 10):
+ #   print(str(i) + " этаж выше, чем " + str(i-1))
 
-print("Дом готов")
+#print("Дом готов")
+
+check = (2*3)
+if check == 6:
+    print("Да ты гений арифметики")
+else: 
+    print("Уа-уа-уа")
